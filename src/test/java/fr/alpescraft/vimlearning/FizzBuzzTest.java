8ff080def65e608ae924d61fc54964f177cc8a5f
Remy Sanlaville <remy.sanlaville@orange.com>
2021-03-05 15:57:05 +0100

    add check environment test

---
+++ src/test/java/fr/alpescraft/vimlearning/FizzBuzzTest.java
@@ -0,0 +1,12 @@
+package fr.alpescraft.vimlearning;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTest {
+
+	@Test
+	public void checkEnvironment(){
+		Assertions.fail("it should failed!");
+	}
+}
